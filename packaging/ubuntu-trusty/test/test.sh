4f1d593a29c02e2c2398113687d70ba7ba31361a
Alan Franzoni <username@franzoni.eu>
2016-05-13 09:45:52 +0200

    Fix: test

--- packaging/ubuntu-trusty/test/test.sh
+++ packaging/ubuntu-trusty/test/test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 cd "${0%/*}"
-
-
 trap '{ set +x; echo; echo FAILED; echo; } >&2' ERR
-
 set -x
 
 dir="$(mktemp -d -t gosu-test-XXXXXXXXXX)"
@@ -17,10 +14,10 @@ docker build -t "$img" "$dir"
 rm -rf "$dir"
 trap - EXIT
 
-trap "docker rm -f '$base' > /dev/null; docker rmi -f '$img' > /dev/null" EXIT
+trap "docker rm -f '$base' > /dev/null; docker rmi -f '$img' > /dev/null;" EXIT
 
 docker run -d --name "$base" "$img" su-exec root sleep 1000
 sleep 1 # give it plenty of time to get through "gosu" and into the "sleep"
-[ "$(docker top "$base" | wc -l)" = 2 ]
+[ "$(docker top $base | wc -l)" -eq 2 ]
 # "docker top" should have only two lines
 # -- ps headers and a single line for the single process running in the container
